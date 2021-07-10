82c2b1faedc79e68c97e5210482626029185432d
Kayode Adechinan <kayode.adechinan@gmail.com>
2021-07-10 21:27:03 +0000

    feat adding browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ const cleanCSS = require("gulp-clean-css");
 const concat = require("gulp-concat");
 const purgecss = require("gulp-purgecss");
 const imagemin = require("gulp-imagemin");
-const sass = require('gulp-sass')(require('sass'));
+const sass = require("gulp-sass")(require("sass"));
+var browserSync = require("browser-sync").create();
 
 gulp.task("minify-css", () => {
   return gulp
@@ -38,15 +39,22 @@ gulp.task("minify-image", () => {
 
 // gulp minify-image
 
-
-
-gulp.task('styles', () => {
-    return gulp.src('src/sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('dist/css'));
+// Static server
+gulp.task("browser-sync", function () {
+  browserSync.init({
+    server: {
+      baseDir: "src",
+    },
+  });
 });
 
-
+gulp.task("sass", () => {
+  return gulp
+    .src("src/sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("src/assets/css"))
+    .pipe(browserSync.stream());
+});
 
 gulp.task("default", gulp.series("minify-css", "minify-image"));
 
@@ -56,6 +64,19 @@ gulp.task("watch", function () {
 });
 */
 
-gulp.task('watch', () => {
-    gulp.watch('src/sass/**/*.scss', gulp.series("styles"));
+gulp.task("watch", () => {
+  gulp.watch("src/sass/**/*.scss", gulp.series("sass"));
 });
+
+// Static Server + watching scss/html files
+gulp.task(
+  "serve",
+  gulp.series("sass", function () {
+    browserSync.init({
+      server: "./src",
+    });
+
+    gulp.watch("src/sass/**/*.scss", gulp.series("sass"));
+    gulp.watch("src/*.html").on("change", browserSync.reload);
+  }),
+);
